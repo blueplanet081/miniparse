54ea7119eeebbe5aaa481d4727b97c237c9d73bb
blueplanet <blueplanet081@outlook.com>
2020-09-03 17:25:16 +0900

    解析処理を大幅変更した改訂暫定版

--- miniparse.py
+++ miniparse.py
@@ -1,14 +1,14 @@
-from typing import TextIO, Union, List, Dict, Tuple, Optional
+from typing import TextIO, Union, List, Dict, Tuple, Optional, Iterator
 import sys
 import pathlib
-from enum import Enum
+from enum import Enum, auto
 import platform
 import glob
 
 '''
 miniparse.py コマンドライン解析モジュール
-version 0.6ぐらい
-2020/9/1 by te.
+version 0.9ぐらい、一部機能を外した暫定版
+2020/9/3 by te.
 '''
 
 ''' <:=>は外したので、Python 3.7 でも動きます。それより前は試していません。 '''
@@ -186,147 +186,36 @@ Es = {'E0': Eset("Command argument expected"),
       }
 
 
-# -------------------------------------------------------------
-# コマンドライン解析用classの定義
-# あらかじめコマンドライン全体をコピーし、順次解析用に使う
-# -------------------------------------------------------------
-
-class Args():
-    ''' コマンドライン解析用class
+def __getWild(wild: str) -> List[str]:
+    ''' ホームディレクトリ '~'、ワイルドカードを展開して、リストで返す。
+        頭にハイフンが付いているもの、ワイルドカードに展開できないものはそのまま返す
+        （その時は、文字列が一つのリストになる）
     '''
-    @staticmethod
-    def getWild(wild: str) -> List[str]:
-        ''' ホームディレクトリ '~'、ワイルドカードを展開して、リストで返す。
-            頭にハイフンが付いているもの、ワイルドカードに展開できないものはそのまま返す
-            （その時は、文字列が一つのリストになる）
-        '''
-        ret = []
-        if wild.startswith('-'):
-            ret.append(wild)
-            return ret
-        if wild.startswith('~'):
-            wild = str(pathlib.Path.home()) + wild[1:]
-            print(wild)
-
-        if '*' in wild or '?' in wild or ('[' in wild and len(wild) > 1):
-            for w in glob.glob(wild):
-                ret.append(w)
-            if not ret:
-                ret.append(wild)
-        else:
-            ret.append(wild)
+    ret = []
+    if wild.startswith('-'):
+        ret.append(wild)
         return ret
+    if wild.startswith('~'):
+        wild = str(pathlib.Path.home()) + wild[1:]
+        print(wild)
 
-    @staticmethod
-    def wildArgs(__args: List[str]) -> List[str]:
-        ''' 渡されたリスト中の項目を、ワイルドカード展開する
-        '''
-        ret = []
-        for p in __args:
-            ret += Args.getWild(p)
-        return ret
+    if '*' in wild or '?' in wild or ('[' in wild and len(wild) > 1):
+        for w in glob.glob(wild):
+            ret.append(w)
+        if not ret:
+            ret.append(wild)
+    else:
+        ret.append(wild)
+    return ret
 
-    def __init__(self, args: List[str]):
-        ''' コマンドライン解析用インスタンスを作成
-        '''
-        self.__cmdPath = args[0]    # コマンドパス名
-        if isWin:
-            self.__args = Args.wildArgs(args[1:])    # 解析用コマンドパラメータ
-        else:
-            self.__args = args[1:]              # 解析用コマンドパラメータ
-        self.__num = 0              # 解析中ブロックポインタ
-        self.__pos = 0              # 解析中ブロック内ポインタ
 
-    def getCommandPath(self) -> str:
-        ''' コマンドパス名を返す
-        '''
-        return self.__cmdPath
-
-    def getCommandName(self) -> str:
-        ''' コマンド名を返す
-        '''
-        p = pathlib.Path(self.__cmdPath)
-        return p.name
-
-    def popOptionType(self) -> str:
-        ''' オプションブロックのタイプを調べる
-        '''
-        if self.__args[self.__num].startswith('--'):
-            self.__pos = 2
-            return '--'         # ロングオプションブロック
-        elif self.__args[self.__num].startswith('-'):
-            self.__pos = 1
-            return '-'          # ショートオプションブロック
-        else:
-            self.__pos = 0
-            return ''           # コマンドアーギュメントブロック
-
-    def popOne(self) -> str:
-        ''' シングルオプションブロックから１文字取り出す
-        '''
-        tpos = self.__pos
-        self.__pos += 1
-        # print(tpos, self.__num)
-        return self.__args[self.__num][tpos:tpos + 1]
-
-    def popLong(self) -> str:
-        ''' ロングオプションブロックから、オプションを取り出す
-        '''
-        tpos = self.__pos
-        # if (ppos := self.__args[self.__num][tpos:].find('=')) == -1:
-        ppos = self.__args[self.__num][tpos:].find('=')
-        if ppos == -1:
-            self.__pos = len(self.__args[self.__num])
-            return self.__args[self.__num][tpos:]
-        else:               # 後ろに '=xxx' のオプションパラメータあり
-            self.__pos = tpos + ppos + 1
-            return self.__args[self.__num][tpos:tpos + ppos]
-
-    def popAllblock(self) -> str:
-        ''' 引数ブロックの全体、または残りを取り出す
-        '''
-        lpos = self.__pos
-        self.__pos = len(self.__args[self.__num])
-        return self.__args[self.__num][lpos:]
-
-    def popOpArg(self) -> str:
-        ''' 次のブロックのオプションパラメータを取得する
-        '''
-        if self.transNext():
-            if self.popOptionType() == '--' and self.isEndBlock():
-                if self.transNext():
-                    return self.popAllblock()
-                else:
-                    return ""
-            elif self.popOptionType() == '':
-                return self.popAllblock()
-            # 次のブロックが無かったり、パラメータブロックだったら取得失敗
-        return ""
-
-    def getRemainArgs(self) -> List[str]:
-        ''' 解析中のコマンドラインの残りを返す（解析位置等は更新されない）
-        '''
-        return self.__args[self.__num:]
-
-    def transNext(self) -> bool:
-        ''' 次の引数ブロックに移行する。
-            途中でも強制的に次に移行するので注意。
-            次が無かったらFalseを返す
-        '''
-        self.__num += 1
-        self.__pos = 0
-        return self.__num < len(self.__args)
-
-    def isEndBlock(self) -> bool:
-        ''' 現行ブロックの終わりならTrue。
-            すべてのブロックが終了していてもTrue
-        '''
-        return self.isAllEnd() or self.__pos >= len(self.__args[self.__num])
-
-    def isAllEnd(self) -> bool:
-        ''' すべてのブロックが終了
-        '''
-        return self.__num >= len(self.__args)
+def wArgs(__args: List[str]) -> List[str]:
+    ''' 渡されたリスト中の項目を、ワイルドカード展開する
+    '''
+    ret = []
+    for p in __args:
+        ret += __getWild(p)
+    return ret
 
 
 # -------------------------------------------------------------
@@ -345,80 +234,102 @@ def arguments() -> List[str]:   # noqa
 
 TparseError = Optional[Tuple[str, ...]]     # エラーType
 
-# def miniparse(tm: 'Args', ops: Dict[str, Oset]) -> TparseError:  # noqa
-def miniparse(args: Args, ops: Topset) -> TparseError:  # noqa
+
+class Ptype(Enum):  # パーツタイプ
+    ''' コマンドライン解析用パーツタイプ
+        (BLOCK, SOPT, SSEP, LOPT)
+    '''
+    BLOCK = auto()      # 普通のブロック
+    SOPT = auto()       # ショートオプション
+    SSEP = auto()       # ショートオプションの終わり
+    LOPT = auto()       # ロングオプション
+
+
+def getOps(__args: List[str]) -> Iterator[Tuple[int, Ptype, str]]:
+    ''' コマンドラインを解析用パーツに分解してタプルで返す。
+        （元のリスト番号, Ptype, コンテンツ）
+    '''
+    i_args = iter(__args)
+    for i, ob in enumerate(i_args):
+        if ob == '--':                      # 強制ブロック（'--' の次のブロック）
+            ob = next(i_args, '')
+            yield i, Ptype.BLOCK, ob
+        elif ob.startswith('--'):           # ロングオプションブロック
+            yield i, Ptype.LOPT, ob[2:]
+        elif ob.startswith('-'):            # ショートオプションブロック
+            sblock = ob[1:]
+            for pp in sblock:               # 分解されたショートオプション
+                yield i, Ptype.SOPT, pp
+            yield i, Ptype.SSEP, sblock     # ショートオプションの終わり
+        else:                               # 普通のブロック
+            yield i, Ptype.BLOCK, ob
+
+
+def miniparse(args: List[str,], ops: Topset) -> TparseError:  # noqa
     ''' コマンドライン解析用モジュール
     '''
     err: TparseError = None
-    while not args.isAllEnd():
-        if args.popOptionType() == '-':     # シングルオプションブロック
-            # while p := args.popOne():
-            while True:
-                p = args.popOne()
-                if not p:
-                    break
-                if p in ops:
-                    ops[p].isTrue = True
-                    if ops[p].needArg:      # オプションパラメータ必要
-                        # if op := args.popAll():
-                        op = args.popAllblock()
-                        if op:
-                            ops[p].opArg = op
-                        # elif op := args.popOpArg():
-                        else:
-                            op = args.popOpArg()
-                            if op:
-                                ops[p].opArg = op
-                            else:
-                                err = ('E2', p)     # E2: Argument expected for the -<p>
-                        break
-                else:
-                    err = ('E1', p)     # E1: Unknown option: -<p>
-                    args.popAllblock()  # エラーなので、ブロックの残りを空読み
-                    break
+    is_needArg = False
+    is_needArgBlock = False
+    needArgP = ''
+    print(args)
 
-        # elif args.optionType() == '--' and (p := args.popLong()):   # ロングオプションブロック
-        elif args.popOptionType() == '--':
-            p = args.popLong()
-            if p:
-                if p in ops:
-                    ops[p].isTrue = True
-                    # if op := args.popAllblock():  # ブロックの後ろがまだあった -->'=xxxx' が付いてた
-                    op = args.popAllblock()
-                    if op:
-                        ops[p].opArg = op       # '=xxxx' の 'xxxx' をパラメータに格納
+    for i, ptype, p in getOps(args):
+        print(i, ptype, p)
+        if is_needArgBlock:                 # オプション引数のブロック待ちだった
+            if ptype == Ptype.BLOCK:            # 普通ブロック来た
+                ops[needArgP].opArg = p
+                is_needArgBlock = False
+            else:                               # 普通ブロックが来なかった
+                err = ('E2', p)     # E2: Argument expected for the -<p>
+                break
+            continue
 
-                    elif ops[p].needArg:        # オプションパラメータ必要
-                        # if op := args.popOpArg():   # 次のブロックをオプションパラメータとして読む
-                        op = args.popOpArg()
-                        if op:
-                            ops[p].opArg = op
-                        else:
-                            err = ('E2', p)     # E2: Argument expected for the -<p>
-                            break
-                else:
-                    err = ('E1', p)     # E1: Unknown option: -<p>
-
-        else:                       # コマンドパラメータブロック
-            if args.isEndBlock():       # 多分前のブロックが '--' だった
-                args.transNext()
-                if args.isAllEnd():         # と思ったら次のブロックが無かった
-                    break
-            param = args.popAllblock()  # コマンドアーギュメント取得
-            __arguments.append(param)
+        if ptype == Ptype.BLOCK:            # 普通のブロック
+            __arguments.append(p)               # コマンド引数として格納
             if '' in ops:
                 ops[''].isTrue = True
-                ops[''].opArg = param
+                ops[''].opArg = p
 
-            if partial_mode == Pmode.PARTIAL:   # 個別モードだったらここで一旦終了
-                args.transNext()
-                return err
+        elif ptype == Ptype.SOPT:           # ショートオプションブロック
+            if is_needArg:                      # オプション引数待ち
+                pass
+            elif p in ops:                      # オプション判定
+                ops[p].isTrue = True
+                if ops[p].needArg:                  # オプション引数必要
+                    is_needArg = True               # オプション引数待ちをセット
+                    needArgP = p
+                    ops[needArgP].opArg = ''
+            else:                               # Unknown option
+                err = ('E1', p)     # E1: Unknown option: -<p>
+                break
 
-        if args.isEndBlock():
-            args.transNext()
+        elif ptype == Ptype.SSEP:           # ショートオプションブロックの終わり
+            if is_needArg:                  # オプション引数待ち
+                ops[needArgP].opArg = p[p.find(needArgP)+1:]
+                if ops[needArgP].opArg:     # オプション引数あった
+                    is_needArg = False
+                else:
+                    is_needArgBlock = True
 
-        if err:
-            break
+        elif ptype == Ptype.LOPT:           # ロングオプションブロック
+            ppos = p.find('=')
+            if ppos != -1:                  # 引数（option=arg）あり
+                pwork = p[ppos+1:]
+                p = p[:ppos]
+                print(p, pwork)
+            else:                           # 引数なし
+                pwork = ''
+            if p in ops:                    # オプション判定
+                ops[p].isTrue = True
+                ops[p].opArg = pwork
+                if ops[p].needArg and not pwork:    # オプション引数必要
+                    is_needArgBlock = True
+                    needArgP = p
+            else:                           # Unknown option
+                err = ('E1', p)     # E1: Unknown option: -<p>
+                print("オプション p はダメ！！")
+                break
 
     if not err:     # 今までエラーが無くて、
         if '' in ops and ops[''].needArg and not ops[''].opArg:
@@ -428,11 +339,13 @@ def miniparse(args: Args, ops: Topset) -> TparseError:  # noqa
     if err:     # エラー検出時処理
         if error_mode in (Emode.ERROR_CONT, Emode.ERROR_END):
             print(Eset.Errmsg(err), file=error_output)
-            printUsage(args.getCommandName(), ops, usage_mode, error_output)
+            # printUsage(args.getCommandName(), ops, usage_mode, error_output)
         if error_mode == Emode.ERROR_END:
             sys.exit(error_code)
 
     return err
+
+
 # -------------------------------------------------------------
 # 定義終了
 # -------------------------------------------------------------
@@ -452,12 +365,12 @@ if __name__ == "__main__":
           'verbose': Oset(False, True, 'あれの長さ', '静かにね'),
           }
 
-    myArgs = Args(sys.argv)
-    err = miniparse(myArgs, pm)
+    myArgs = wArgs(sys.argv)
+    err = miniparse(myArgs[1:], pm)
 
     if pm['h'].isTrue:
         print('このプログラムは、モジュール miniparse の使い方を示すものです。')
-        printUsage(myArgs.getCommandName(), pm, Umode.USAGE_AND_OPTION)
+        # printUsage(myArgs.getCommandName(), pm, Umode.USAGE_AND_OPTION)
 
     print()
     print('----')
